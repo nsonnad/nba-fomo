002eaf9d75cdc5ea41fa175676733784a4b1eedc
nikhil s <nikhil@nsnnd.com>
2025-12-12 21:27:52 -0500

    something to look at

--- src/lib/Game.svelte
+++ src/lib/Game.svelte
@@ -1,10 +1,30 @@
 <script lang="ts">
+	import { scaleSequential, interpolateRdYlGn, scaleOrdinal, schemeRdYlGn, format } from 'd3';
 	export let game;
+
+	const colorScale = scaleSequential(interpolateRdYlGn).domain([25, 100]);
+	const formatDec = format('.2f');
+	const gameColor = colorScale(game.watchability_score);
 	console.log(game);
 </script>
 
-<div>
-	<span>{game.away_team}</span>
-	<span>vs.</span>
-	<span>{game.home_team}</span>
-</div>
+<tr style="background-color: {gameColor};">
+	<td>{game.gameDate.getMonth() + 1}/{game.gameDate.getDate() + 1}</td>
+	<td>{game.away_team}</td>
+	<td>@</td>
+	<td>{game.home_team}</td>
+	<td class="number">{formatDec(game.watchability_score)}</td>
+	<td class="number">{game.netrtg_sum}</td>
+	<td class="number">{game.netrtg_diff}</td>
+	<td class="number">{formatDec(game.away_team_health)}</td>
+	<td class="number">{formatDec(game.home_team_health)}</td>
+</tr>
+
+<style>
+	td {
+		padding: 6px 10px;
+	}
+	td.number {
+		text-align: right;
+	}
+</style>
--- src/lib/Matchups.svelte
+++ src/lib/Matchups.svelte
@@ -4,7 +4,35 @@
 </script>
 
 <div>
-	{#each games as game}
-		<Game {game} />
-	{/each}
+	<table>
+		<tbody>
+			<tr>
+				<th>date</th>
+				<th>away</th>
+				<th></th>
+				<th>home</th>
+				<th>watchability</th>
+				<th>netrtg</th>
+				<th>diff</th>
+				<th>a_health</th>
+				<th>h_health</th>
+			</tr>
+			{#each games as game}
+				<Game {game} />
+			{/each}
+		</tbody>
+	</table>
 </div>
+
+<style>
+	table {
+		font-family: monospace;
+		font-size: 1rem;
+		border-collapse: collapse;
+	}
+
+	th {
+		text-align: left;
+		padding: 6px 10px;
+	}
+</style>
